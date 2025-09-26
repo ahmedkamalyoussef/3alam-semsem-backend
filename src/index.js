d95c49218196b8ff3d889438df9da436512f0410
ahmedkamalyoussef <ahmedkamalyoussef4@gmail.com>
2025-09-27 02:44:32 +0300

    edit index

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ app.use((err, req, res, next) => {
 });
 
 // Database sync
-const PORT = process.env.PORT || 5001;
+//const PORT = process.env.PORT || 5001;
 
 // sequelize.sync({ alter: true })
 //   //.sync({})
